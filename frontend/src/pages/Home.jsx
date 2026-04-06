03034d607ade997563914bf6390dedb9f52db6da
Rishav Sharma <rs4877246@gmail.com>
2026-04-06 15:34:51 +0530

    Add and update pages: Home, About, Login, Signup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import Hero from '@/components/Hero'
+import React from 'react'
+import RecentBlog from '@/components/RecentBlog'
+import PopularAuthors from '@/components/PopularAuthors'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='pt-20'>
+      <Hero/>
+      <RecentBlog/>
+      <PopularAuthors/>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
