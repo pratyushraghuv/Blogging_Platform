f0f311aaafad8f0c2a254f3c08867c9504878e97
Rishav Sharma <rs4877246@gmail.com>
2026-04-11 09:34:17 +0530

    Add and update pages and lib folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,104 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import Blogs from './pages/Blogs';
-import About from './pages/About';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import React from 'react'
+import Signup from './pages/Signup'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
+import Profile from './pages/Profile'
+import Blog from './pages/Blog'
+import CreateBlog from './pages/CreateBlog'
+import Dashboard from './pages/Dashboard'
+import YourBlog from './pages/YourBlog'
+import BlogView from './pages/BlogView'
+import Footer from './components/Footer'
+import About from './pages/About'
+import Comments from './pages/Comments'
+import UpdateBlog from './pages/UpdateBlog'
+import ProtectedRoute from './components/ProtectedRoute'
+import SearchList from './pages/SearchList'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<><Navbar/><Home/></>,
+    element: <><Navbar/><Home /><Footer/></>
   },
   {
     path: "/blogs",
-    element: <><Navbar/><Blogs/></>,
+    element: <><Navbar/><Blog /><Footer/></>
   },
   {
     path: "/about",
-    element: <><Navbar/><About/></>,
+    element: <><Navbar/><About /><Footer/></>
+  },
+  {
+    path: "/search",
+    element: <><Navbar/><SearchList/><Footer/></>
+  },
+  {
+    path: "/blogs/:blogId",
+    element: <><Navbar/><ProtectedRoute><BlogView /></ProtectedRoute></>
+  },
+  {
+    path: "/write-blog",
+    element: <><Navbar/><CreateBlog /></>
+  },
+ 
+  {
+    path: "/profile",
+    element: <><Navbar/><Profile /></>
+  },
+  // {
+  //   path: "write-blog/:blogId",
+  //       element: <><Navbar/><CreateBlog /></>
+  // },
+  // {
+  //   path: "/dashboard",
+  //   element: <><Navbar/><Dashboard /></>
+  // },
+  {
+    path:"/dashboard",
+    element: <><Navbar/><ProtectedRoute><Dashboard/></ProtectedRoute></>,
+    children:[
+      {
+        path: "write-blog",
+        element:<><CreateBlog/></>
+      },
+      {
+        path: "write-blog/:blogId",
+        element: <><UpdateBlog /></>
+      },
+      {
+        path: "your-blog",
+        element:<YourBlog/>
+      },
+      {
+        path: "comments",
+        element:<Comments/>
+      },
+      {
+        path: "profile",
+        element:<Profile/>
+      },
+      
+      
+    ]
+   },
+  {
+    path: "/signup",
+    element: <><Navbar/><Signup /></> 
   },
   {
     path: "/login",
-    element: <><Navbar/><Login/></>,
+    element: <><Navbar/><Login /></>
   },
-  {
-    path: "/signup",
-    element: <><Navbar/><Signup/></>,
-  }
-]);
+])
 
 const App = () => {
   return (
-   <>
-     <RouterProvider router={router} />
-   </>
-  );
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
 }
 
-export default App;
+export default App
